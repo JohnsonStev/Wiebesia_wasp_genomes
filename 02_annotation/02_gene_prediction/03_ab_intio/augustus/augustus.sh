6e982b843a9efdfc370807786b8fddb5a8a98172
Bai-Wei Lo <38938938+JohnsonStev@users.noreply.github.com>
2023-11-01 22:11:57 +0100

    Create augustus.sh

---
+++ 02_annotation/02_gene_prediction/03_ab_intio/augustus/augustus.sh
@@ -0,0 +1,3 @@
+
+augustus --strand=both --genemodel=partial --singlestrand=true --gff3=on --progress=true --UTR=off --hintsfile=JW.hints.gff --noInFrameStop=true  --extrinsicCfgFile=extrinsic.bug.cfg --uniqueGeneId=true --species=BUSCO_busco_gatk_2462183146 $genome
+
